386f73f24b3da0a7e874b4f774338d44438e8f00
Irvin Cortez <irvincotez67@gmail.com>
2024-01-16 09:05:35 -0600

    add function and remove function

---
+++ TaskCosmoDB/Data/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskCosmoDB.Data
+{
+    public class Task
+    {
+        public string Id { get; set; }
+        public int sort {  get; set; } = 0;
+        public string Description { get; set; }
+        public bool Status { get; set; } = false;
+
+    }
+}
